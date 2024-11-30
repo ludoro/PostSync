50988f6ed57c1d7df26b5d311789a20d9292bda7
Ludovico Bessi <ludovicobessi@gmail.com>
2024-11-30 16:13:22 +0100

    .

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -39,7 +39,7 @@ interface PostFormProps {
   setContent: (content: string) => void;
 }
 
-type PostStatus = 'draft' | 'scheduled';
+type PostStatus = 'draft' | 'scheduled' | 'published';
 type FilePreview = {
   file: File
   preview: string
